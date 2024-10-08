6571e4af86a84c62755fe095f64e7ec59624f1fb
Douglas Shinzato <shinzato.douglas@gmail.com>
2024-10-08 18:12:05 -0400

    fastify port fix

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,12 @@ app.register(statesRoutes, {
   prefix: '/',
 })
 
+const port = process.env.PORT || 3333
+
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('Server Running!')
