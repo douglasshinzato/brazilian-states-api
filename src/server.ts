1cd2098ad8dd19b06b309de1a21c7fda608993d0
Douglas Shinzato <shinzato.douglas@gmail.com>
2024-10-08 18:04:04 -0400

    main route fix

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,12 @@ import { statesRoutes } from './routes/states'
 const app = fastify()
 
 app.register(statesRoutes, {
-  prefix: 'states',
+  prefix: '/',
 })
 
 app
   .listen({
-    port: 3334,
+    port: 3333,
   })
   .then(() => {
     console.log('Server Running!')
